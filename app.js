08b33f99762a1e88b104248502d659de4955e955
vexscrewe <monokumaphu@gmail.com>
2025-07-04 22:03:21 -0400

    AtualizaçãoConexãoHTML

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1', require('./routes'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
